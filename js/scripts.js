0a192a990eab610204af8599a0cf2d75bdd6b360
Ian Kabugi <iank299@gmail.com>
2018-12-18 08:38:34 +0300

    change the function in js to make the code to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("hi")
+$(document).ready(function(){
+  $("#roll1").click(function() {
+
+    var roll1=document.getElementById(roll1).val();
+    var score1 =document.getElementById(score1).val();
+    var d1=Math.Int(Math.random()* 6)+1;
+    alert(roll1);
+  }
+});
